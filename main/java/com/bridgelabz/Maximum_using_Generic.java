b473245eb0a78d94a099c6e3c5ea006182c7d844
Shivam Mole <shivammole1713@gmail.com>
2024-09-24 22:56:20 +0530

    Finding maximum using generic via float

--- main/java/com/bridgelabz/Maximum_using_Generic.java
+++ main/java/com/bridgelabz/Maximum_using_Generic.java
@@ -15,10 +15,24 @@ public class Maximum_using_Generic {
         return max;
     }
 
+    public static Float find_max(Float num1,Float num2,Float num3){
+
+        Float max=num1;
+
+        if(num2.compareTo(max)>0)
+            max=num2;
+        if(num3.compareTo(max)>0)
+            max=num3;
+
+        return max;
+    }
+
     public static void main(String[] args) {
 
         Integer num1=10,num2=12,num3=90;
         System.out.printf("Maximum of %d %d %d is :- %d",num1,num2,num3,find_max(num1,num1,num3));
 
+        Float result = find_max(10.5f, 5.2f, 3.3f);
+        System.out.println("Maximum Float is :- " + result);
     }
 }
--- test/java/com/bridgelabz/Maximum_using_GenericTest.java
+++ test/java/com/bridgelabz/Maximum_using_GenericTest.java
@@ -27,4 +27,24 @@ class Maximum_using_GenericTest {
         assertEquals(num3,maxi1.find_max(num1,num2,num3),"Max should be at osition Third");
     }
 
+    @Test
+    public void testMaximumAtFirstPositionFloat(){
+        Float num1=12.2f,num2=3.1f,num3=9.3f;
+        assertEquals(num1,maxi1.find_max(num1,num2,num3),"Maximum number should be at first position");
+
+    }
+
+    @Test
+    public void testMaxiAtSecondPositionFloat(){
+        Float num1=11.2f,num2=13.1f,num3=3.3f;
+        assertEquals(num2,maxi1.find_max(num1,num2,num3),"Max should be at osition second");
+    }
+
+    @Test
+    public void testMaxiAtThirdPositionFloat(){
+        Float num1=10.2f,num2=23.1f,num3=29.3f;
+        assertEquals(num3,maxi1.find_max(num1,num2,num3),"Max should be at osition Third");
+    }
+
+
 }
